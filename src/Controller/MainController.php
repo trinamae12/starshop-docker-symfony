6bc47a565a68cf5689390ef1503851cb0399686c
trinamae12 <maetrina12@gmail.com>
2024-09-11 12:05:58 +0800

    Add templates using twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+        $starshipCount = 457;
+
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'Under Construction'
+        ];
+
+        return $this->render('main/homepage.html.twig',[ 
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip
+        ]);
     }
 }
