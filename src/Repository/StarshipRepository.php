c13a318b08b5729a52f29723e31305e88bad3e7c
trinamae12 <maetrina12@gmail.com>
2024-09-11 15:40:06 +0800

    Add enum for status

--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Model\Starship;
+use App\Model\StarshipStatusEnum;
 use Psr\Log\LoggerInterface;
 
 class StarshipRepository
@@ -21,21 +22,21 @@ class StarshipRepository
                 'USS LeafyCruiser (NCC-0001)',
                 'Garden',
                 'Jean-Luc Pickles',
-                'under construction'
+                StarshipStatusEnum::IN_PROGRESS
             ),
             new Starship(
                 2,
                 'USS Nespresso (NCC-1234-C)',
                 'Latte',
                 'James T. Quick!',
-                'repaired'
+                StarshipStatusEnum::COMPLETED
             ),
             new Starship(
                 3,
                 'USS Wanderlust (NCC-2024-W)',
                 'Delta Tourist',
                 'Kathryn Journeyway',
-                'under construction'
+                StarshipStatusEnum::WAITING
             )];
     }
 
