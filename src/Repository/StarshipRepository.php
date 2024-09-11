1287666f74a72cb7e3ac7f7ee4699bad35176b11
trinamae12 <maetrina12@gmail.com>
2024-09-11 14:27:10 +0800

    Add models and repositories

---
+++ src/Repository/StarshipRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repository;
+
+use App\Model\Starship;
+use Psr\Log\LoggerInterface;
+
+class StarshipRepository
+{
+    public function __construct(private LoggerInterface $logger)
+    {
+
+    }
+
+    public function findAll(): array 
+    {
+        $this->logger->info('Starship collection retrieved');
+        return [
+            new Starship(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                'under construction'
+            ),
+            new Starship(
+                2,
+                'USS Nespresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                'repaired'
+            ),
+            new Starship(
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'under construction'
+            )];
+    }
+
+    // returns nullable Starship object
+    public function find(int $id): ?Starship
+    {
+        foreach($this->findAll() as $starship) {
+            if($starship->getId() === $id) {
+                return $starship;
+            }
+        }
+        
+        return null;
+    }
+}
